0bdafcca43c7f473fd938eddbc9a9b3fb8aeb308
fin <2581613136@qq.com>
2025-01-22 19:37:48 +0800

    modify

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Footer() {
   return (
     <footer className=" text-xl h-16 flex justify-center items-center dark:bg-slate-900  dark:text-white">
